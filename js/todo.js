c40b705ec38a8c97012ded5af5dce8b9ed4d6517
Jiyeon <tjdwldus95@naver.com>
2023-05-01 13:10:37 +0900

    0501 TODO

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,10 @@ const todoForm = document.querySelector("#todo-form");
 const todoInput = document.querySelector("#todo-form input")
 const todoList = document.querySelector("#todo-list");
 
-// 입력되는 newTodo를 저장할 배열 생성
+// 입력되는 newTodo를 저장할 배열 생성 -> 변경 가능하도록 let으로 선언
 // 새로고침하면 작성한 todo가 초기화되고 있기 때문에
 // localStorage를 활용하여 작성한 todo값 저장
-const todos = [];
+let todos = [];
 
 // saveTodos라는 문자를 여러 번 입력하기 때문에 대문자로 정의 -> 실수를 줄이기 위함
 const TODO_KEY = "saveTodos";
@@ -22,20 +22,30 @@ function saveTodos() {
 
 // todo를 삭제하는 역할 담당 함수 작성
 function deleteTodo(event) {
+    // button을 눌렀을 때 deleteTodo가 실행되도록 EventListener를 작성
+    // button의 부모요소(parentElement)가 li이기 때문에 아래와 같이 작성
     const li = event.target.parentElement;
     li.remove();
+    
+    // 특정 버튼을 클릭하면 해당 버튼의 부모요소인 li를 삭제한 뒤 새로운 배열을 생성
+    // filter를 이용해 삭제된 li의 id와 동일하지 않은 요소만 남긴 새로운 배열 todos 생성
+    todos = todos.filter(todo => todo.id !== parseInt(li.id));
+
+    // 새로운 배열 todos를 다시 localStorage에 저장하기 위해 saveTodos 함수 호출
+    saveTodos();
 }
 
-// todo를 그리는 역할 담당 함수 작성
-// handleTodoSubmit에서 newTodo를 전달받아 사용
-function paintTodo(newTodo) {
+// todo를 그리는(화면에 표시하는) 역할 담당 함수 작성
+// handleTodoSubmit에서 newTodoObj를 전달받아 사용
+function paintTodo(newTodoObj) {
     // todoList(ul태그) 안에 들어갈 li 생성
     const li = document.createElement("li");
+    li.id = newTodoObj.id;
 
     // li태그 안에 들어갈 span태그 생성
     const span = document.createElement("span");
-    // span태그 안의 텍스트를 newTodo로 변경
-    span.innerHTML = newTodo;
+    // span태그 안의 텍스트를 newTodoObj의 text로 변경
+    span.innerHTML = newTodoObj.text;
 
     // li태그 안에 들어갈 button태그 생성
     const button = document.createElement("button");
@@ -64,13 +74,22 @@ function handleTodoSubmit(event) {
     // submit이 실행되면 입력창이 비워지도록 ""값 작성
     todoInput.value = "";
 
-    // todos배열에 newTodo를 전달(push)
-    todos.push(newTodo);
+    // newTodo를 배열이 아닌 object로 만들기 위해 newTodoObj 생성
+    // object로 만드는 이유: id값을 부여하기 위해서
+    const newTodoObj = {
+        text: newTodo,
+        // Date.now(): 밀리초를 값으로 주는 함수
+        // ↓ 아래와 같이 작성하면 id로 랜덤한 숫자를 얻을 수 있다
+        id: Date.now(),
+    };
 
-    // newTodo를 paintTodo함수에 전달
-    paintTodo(newTodo);
+    // todos배열에 newTodoObj를 전달(push)
+    todos.push(newTodoObj);
 
-    // saveTodos 실행
+    // newTodoObj를 paintTodo함수에 전달
+    paintTodo(newTodoObj);
+
+    // saveTodos 실행 -> 저장
     saveTodos();
 }
 
@@ -86,4 +105,10 @@ if(savedTodos !== null) {
     // savedTodos가 null이 아니면 savedTodos에 저장된 string을 array로 변경
     // JSON.parse(): string을 array로 변경하는 기능
     const parsedTodos = JSON.parse(savedTodos);
+
+    // todos 배열에 parsedTodos 저장
+    todos = parsedTodos;
+
+    // forEach(): parsedTodos에 있는 각각의 요소마다 paintTodo함수를 실행
+    parsedTodos.forEach(paintTodo);
 }
